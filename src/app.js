1b12632f5be4fac8132b7d6721f10ff401fcf88f
Vadym Sainiuk <vadik.sajnuykk@gmail.com>
2024-04-09 17:10:16 +0300

    connected DB

--- src/app.js
+++ src/app.js
@@ -13,6 +13,7 @@ app.use('/api/tasks', taskRoutes);
 app.use('/api/users', userRoutes);
 
 const PORT = process.env.PORT || 3000;
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
