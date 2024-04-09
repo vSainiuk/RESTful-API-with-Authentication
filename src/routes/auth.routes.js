ad46fe7d47a8b025a0a35396065e069f7afb8a75
Vadym Sainiuk <vadik.sajnuykk@gmail.com>
2024-04-09 20:42:28 +0300

    fixed issue

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { register, login } = require('../controllers/user.controller');
+const { register, login } = require('../controllers/auth.controller');
 
 router.post('/register', register);
 router.post('/login', login);
